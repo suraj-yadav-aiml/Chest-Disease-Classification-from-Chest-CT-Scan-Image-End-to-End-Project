28b942fee2243c5cc5b206c4eb466a9c696ad185
Suraj Yadav <qwertypad7612@gmail.com>
2024-07-11 16:11:04 +0530

    fix bug in common.py

--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -59,7 +59,7 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
 
 
 @ensure_annotations
-def create_directories(paths_to_directories: list, verbose: bool = True) -> None:
+def create_directories(paths_to_directories: list, verbose: bool = True):
     """
     Creates directories specified by a list of paths.
 
